be6b6ef4b5f316131606a3474f9fb4e051bfeabc
Suresh133 <51027983+Suresh133@users.noreply.github.com>
2019-06-10 08:55:50 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -1,31 +1,22 @@
 from pyrogram import Client, Filters
 
-
-app = Client('666639160:AAEtopjBMU5r_i_UROx8PTWSSOtIrU6V7W8',605563,"7f2c2d12880400b88764b9b304e14e0b")
-
-
-
-u = '-1001336546427'
-s = '-1001378725482'
-
-
-@app.on_message(Filters.chat(int(s)) & Filters.text & ~ Filters.edited)
-
-def forawrd(client, message):
-
-   client.send_message(int(u), "**" + message.text + "**")
-           
+app = Client('663574960:AAGWfrBnjGYGSczuGHGLG60RVLMp6ebWteM',605563,"7f2c2d12880400b88764b9b304e14e0b")
 
 
 
+u = '-1001157455913'
 
-@app.on_message(Filters.chat(int(s)) & Filters.sticker)
-
-def forawrd(client, message):
-
-   client.send_sticker(int(u),message.sticker.file_id)
-            
+s =  '-1001171781537'
 
 
+@app.on_message(Filters.chat(int(s))& Filters.text & ~Filters.edited)
+def forward(client, message):
+    
+   client.send_message(int(u), message.text)
+   client.send_message(int(u), message_id)
+
+@app.on_message(Filters.chat(int(s))& Filters.text & ~Filters.edited)
+def forward(client, message):
+   client.send_message(int(u), message_id)
 
 app.run()
