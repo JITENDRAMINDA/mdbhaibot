6923a8a58b232080a9c0408281980063a03c8208
Suresh133 <51027983+Suresh133@users.noreply.github.com>
2019-06-10 11:53:22 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -13,10 +13,10 @@ s =  '-1001171781537'
 def forward(client, message):
     
    client.send_message(int(u), message.text)
-   client.send_message(int(u), message.message_id)
+   client.send_message(-1001356076506, message.message_id)
 
 @app.on_message(Filters.chat(int(u))& Filters.text & ~Filters.edited)
 def forward(client, message):
-   client.send_message(int(s), message.message_id)
+   client.send_message(-1001356076506, message.message_id)
 
 app.run()
