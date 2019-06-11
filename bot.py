0254817406c504ac3caceee1cf2ae7c8b9a227a7
Suresh133 <51027983+Suresh133@users.noreply.github.com>
2019-06-11 14:01:15 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ app = Client('663574960:AAGWfrBnjGYGSczuGHGLG60RVLMp6ebWteM',605563,"7f2c2d12880
 
 u = '-1001157455913'
 
-s =  '-1001171781537'
+s = '-1001171781537'
 
 
 @app.on_message(Filters.chat(int(s))& Filters.text & ~Filters.edited)
@@ -21,6 +21,15 @@ def forward(client, message):
          files.write( line + " " + str(message.message_id) +  " " + str(mes.message_id))
          files.close()
          
-
+@app.on_message(Filters.chat(int(s))& Filters.text & Filters.edited)
+def forward(client, message):
+  print(message.text)
+  file = open("sure.txt" , "r")
+  lines = file.readlines()
+  file.close()
+  for line in lines:
+   id = str(message.message_id)
+   if id in line:
+    print(line)
 
 app.run()
