784e296c4b2a00a647d91d36f91c002f0afb7ae0
Suresh133 <51027983+Suresh133@users.noreply.github.com>
2019-06-13 21:26:21 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -12,10 +12,8 @@ s = '-1001171781537'
 
 @app.on_message(Filters.chat(int(s))& Filters.text & ~Filters.edited)
 def forward(client, message):
-   x = time.time()
-   client.send_message(int(u), message.text + "-" + str(message.message_id) )
-   print("z")
-   time.sleep(1)
+   time.sleep(10)
+   client.send_message(..)
   
        
 
