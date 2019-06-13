a7bf88bbd5ff94ce0d325a6742a4b03d0abf148e
Suresh133 <51027983+Suresh133@users.noreply.github.com>
2019-06-13 20:08:47 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -7,22 +7,16 @@ TOKAN = "663574960:AAGWfrBnjGYGSczuGHGLG60RVLMp6ebWteM"
 
 app = Client( TOKAN ,605563,"7f2c2d12880400b88764b9b304e14e0b")
 
-
-
 u = '-1001157455913'
-
 s = '-1001171781537'
-
-app.add_handler(DeletedMessagesHandler(callable))
 
 
 @app.on_message(Filters.chat(int(s))& Filters.text & ~Filters.edited)
 def forward(client, message):
    x = time.time()
-   While time.time()-x == 30:
-      client.send_message(int(u), message.text + "-" + str(message.message_id) )
-      print("z")
-      time.sleep(1)
+   client.send_message(int(u), message.text + "-" + str(message.message_id) )
+   print("z")
+   time.sleep(1)
   
        
 
@@ -30,18 +24,5 @@ def forward(client, message):
 
 
 
-def main(client, message):
-  file = open("sure.txt" , "r")
-  lines = file.readlines()
-  file.close()
-  z = time.time() - int(x)
-  time.sleep(1)
-  for line in lines:
-   if z < 30:
-      client.send_message(int(u), message.text + "-" + str(message.message_id) )
-      print(z)
-      time.sleep(1)
-
-         
-
+time.sleep(1)
 app.run()
