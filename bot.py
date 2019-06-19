d6cd453d96ed4ec0b9fc2389e985af4bb770bbcd
Suresh133 <51027983+Suresh133@users.noreply.github.com>
2019-06-19 19:27:13 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -4,174 +4,45 @@ TOKAN = "639957559:AAFbwAStH_GXBgUVFxC93CCsbBM5MSA-Piw"
 
 app = Client( TOKAN ,605563,"7f2c2d12880400b88764b9b304e14e0b")
 
-@app.on_message(Filters.command("delete"))
+@app.on_message(Filters.chat(bullet) & ~ Filters.edited)
 def main(client, message):
-  for message.message_id in range(int(message.text.split(' ')[1]), int(message.text.split(' ')[2])):
+ file = open("bullet.txt" , "r")
+ lines = file.readlines()
+ file.close()
+  for line in lines:
+   p = line.split()
+   for x in p:
     try:
-        client.delete_messages(message.chat.id,message.message_id)
+     mes = client.send_message( int(x), "**" + message.text + "**" )
+     fille = open(str(x)+".txt","r")
+     n = fille.readlines()
+     fille.close()
+     for t in n:
+      fie = open(str(x)+".txt","w")
+      fie.write(t +" " + str(message.message_id) + " " + str(mes.message_id))
+      fie.close()
     except:
-        continue
-
-@app.on_message(Filters.channel & ~ Filters.edited)
+     continue
+@app.on_message(Filters.chat(bullet) & Filters.edited)
 def main(client, message):
-  file = open("bullets.txt" , "r")
-  s = file.readlines()
-  file.close()
-  for d in s:
-   if message.chat.id == int(d):
-    file = open("bulleti.txt" , "r")
-    lines = file.readlines()
-    file.close()
-    for line in lines:
-     p = line.split()
-     for x in p:
-       try:
-          mes = client.send_message( int(x), "**" + message.text + "**" )
-          fille = open(str(x)+".txt","r")
-          n = fille.readlines()
-          fille.close()
-          for t in n:
-           fie = open(str(x)+".txt","w")
-           fie.write(t +" " + str(message.message_id) + " " + str(mes.message_id))
-           fie.close()
-       except:
-           continue
-
-  fil = open("ferraris.txt" , "r")
-  q = fil.readlines()
-  fil.close()
-  for e in q:
-   if message.chat.id == int(e):
-    file = open("ferrarii.txt" , "r")
-    lines = file.readlines()
-    file.close()
-    for line in lines:
-     p = line.split()
-     for x in p:
-       try:
-          mes = client.send_message( int(x), "**" + message.text + "**" )
-          fille = open(str(x)+".txt","r")
-          n = fille.readlines()
-          fille.close()
-          for t in n:
-           fie = open(str(x)+".txt","w")
-           fie.write(t +" " + str(message.message_id) + " " + str(mes.message_id))
-           fie.close()
-       except:
-           continue
-  fl = open("sessionss.txt" , "r")
-  h = fl.readlines()
-  fl.close()
-  for e in h:
-   if message.chat.id == int(e):
-    file = open("sessionsi.txt" , "r")
-    lines = file.readlines()
-    file.close()
-    for line in lines:
-     p = line.split()
-     for x in p:
-       try:
-          mes = client.send_message( int(x), "**" + message.text + "**" )
-          fille = open(str(x)+".txt","r")
-          n = fille.readlines()
-          fille.close()
-          for t in n:
-           fie = open(str(x)+".txt","w")
-           fie.write(t +" " + str(message.message_id) + " " + str(mes.message_id))
-           fie.close()
-       except:
-           continue
-
-
-  
-@app.on_message(Filters.channel & Filters.edited)
-def main(client, message):
-  file = open("ferraris.txt" , "r")
-  s = file.readlines()
-  file.close()
-  for d in s:
-   if message.chat.id == int(d):
-    filer = open("update.txt" , "r")
-    m = filer.readlines()
-    filer.close()
-    for l in m:
-     if l == "on":
-      file = open("ferrarii.txt" , "r")
-      lines = file.readlines()
-      file.close()
-      for line in lines:
-       p = line.split()
-       for o in p:
-        files = open(str(o)+".txt" , "r")
-        d = files.readlines()
-        files.close()
-        for c in d:
-         x = c.split()
-         id = str(message.message_id)
-         if id in x:
-           try:
-             client.edit_message_text(int(o),int(x[x.index(id)+1]), "**" + message.text + "**" )
-           except:
-             continue
+ file = open("bullet.txt" , "r")
+ lines = file.readlines()
+ file.close()
+  for line in lines:
+   p = line.split()
+   for o in p:
+    files = open(str(o)+".txt" , "r")
+    d = files.readlines()
+    files.close()
+    for c in d:
+     x = c.split()
+     id = str(message.message_id)
+     if id in x:
+      try:
+       client.edit_message_text(int(o),int(x[x.index(id)+1]), "**" + message.text + "**" )
+      except:
+       continue
           
-
-  fil = open("bullets.txt" , "r")
-  f = fil.readlines()
-  fil.close()
-  for d in f:
-   if message.chat.id == int(d):
-    filer = open("update.txt" , "r")
-    m = filer.readlines()
-    filer.close()
-    for l in m:
-     if l == "on":
-      file = open("bulleti.txt" , "r")
-      lines = file.readlines()
-      file.close()
-      for line in lines:
-       p = line.split()
-       for o in p:
-        files = open(str(o)+".txt" , "r")
-        d = files.readlines()
-        files.close()
-        for c in d:
-         x = c.split()
-         id = str(message.message_id)
-         if id in x:
-           try:
-             client.edit_message_text(int(o),int(x[x.index(id)+1]), "**" + message.text + "**" )
-           except:
-             continue
- 
-  fl = open("sessionss.txt" , "r")
-  f = fl.readlines()
-  fl.close()
-  for d in f:
-   if message.chat.id == int(d):
-    filer = open("update.txt" , "r")
-    m = filer.readlines()
-    filer.close()
-    for l in m:
-     if l == "on":
-      file = open("sessionsi.txt" , "r")
-      lines = file.readlines()
-      file.close()
-      for line in lines:
-       p = line.split()
-       for o in p:
-        files = open(str(o)+".txt" , "r")
-        d = files.readlines()
-        files.close()
-        for c in d:
-         x = c.split()
-         id = str(message.message_id)
-         if id in x:
-           try:
-             client.edit_message_text(int(o),int(x[x.index(id)+1]), "**" + message.text + "**" )
-           except:
-             continue
-                
-     
 
 @app.on_message(Filters.command('add') & Filters.user(491634139) )
 def forward(client, message):
@@ -238,40 +109,7 @@ def forward(client, message):
   for v in u :
       message.reply("🏘️ List of Chat_ids in my database are ```" + str(v) + "```. Its can be change. ✅✅")
   
- 
-@app.on_message(Filters.command('sets') & Filters.user(491634139) )
-def forward(client, message):
- if len(message.text.split(' ')) > 2:
-  if len(message.text.split(' ')[1]) == 14:
-   with open(message.text.split(' ')[2] + '.txt', 'w') as file:
-    file.write(message.text.split(' ')[1])
-    file.close()
-    message.reply("🌐 Done, Now my source chat is ```" + message.text.split(' ')[1] + "```. I will try to forward messages from this chat. ✅✅")
-  else:
-    message.reply("💼 Please write a valid chat id. ✅✅ ")
- else:
-    message.reply("💼 Please write a valid chat id. ✅✅ ")
-
-   
-@app.on_message(Filters.command('setupdate') & Filters.user(491634139) )
-def forward(client, message):
- if len(message.text.split(' ')) > 1:
-   with open('update.txt', 'w') as file:
-    file.write(message.text.split(' ')[1])
-    file.close()
-    message.reply("🌐 Done,Now my message update status is ```" + message.text.split(' ')[1] + "```.✅✅")
- else:
-    message.reply("💼 Please write a valid chat id. ✅✅ ")
-
-
-@app.on_message(Filters.command('source') & Filters.user(491634139) )
-def forward(client, message):
-   with open(message.text.split(" ")[1] + '.txt', 'r') as file:
-    x = file.readlines()
-    file.close()
-    for y in x:
-     message.reply("🌐 My source chat is ```" + y + "```. I am trying to forward messages from this chat. ✅✅")
- 
+    
 @app.on_message(Filters.command('get') & Filters.user(491634139) )
 def forward(client, message):
  if len(message.text.split(' ')) > 1:
@@ -283,26 +121,13 @@ def forward(client, message):
  else:
     message.reply("💼 Please write a valid chat id. ✅✅ ")
 
-@app.on_message(Filters.command('update') & Filters.user(491634139) )
-def forward(client, message):
-    with open('update.txt', 'r') as file:
-     x = file.readlines()
-     file.close()
-     for y in x:
-       message.reply("🌐 My current message update status is ```" + y + "```. ✅✅")
- 
 @app.on_message(Filters.command("start"))
 def forward(client, message):
  if message.from_user.id == 491634139:
    message.reply("♻️ Welcome to your LineBot . ✅✅")
  else:
    message.reply("♻️ You need admins permission to use my functions. ✅✅")
-  
-  
-  
-
-                 
-    
+     
 @app.on_message(Filters.private)
 def forward(client, message):
  if not message.from_user.id == 491634139:
